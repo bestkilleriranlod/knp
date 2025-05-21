565d1a359d063577dbdcc57d03d794db404e8b68
bestkilleriranlod <bestkilleriranlod@gmail.com>
2025-05-21 16:14:24 +0330

    آپلود کامل همه فایل‌ها

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var AdmZip = require("adm-zip");
 var { accounts_clct, panels_clct, users_clct, logs_clct } = require('./db_interface');
 const amnezia_sub_page_html = fs.readFileSync("custom_sub/amnezia.html").toString();
 const not_found_page_html = fs.readFileSync("custom_sub/404.html").toString();
-var AMNEZIA_COEFFICIENT = 2.33;
+var AMNEZIA_COEFFICIENT = 2;
 if(process.env.RELEASE == "ALI") AMNEZIA_COEFFICIENT = 10;
 var SD_VARIABLE = 0;
 
@@ -386,7 +386,7 @@ app.post("/create_user", async (req, res) => {
 
      // TEMP
      ip_limit = 5;
-     if(process.env.RELEASE == "ALI") ip_limit = 1;
+     if(process.env.RELEASE == "ALI") ip_limit = 3;
 
 
         if(process.env.RELEASE == "ARMAN") flow_status = "xtls-rprx-vision";
