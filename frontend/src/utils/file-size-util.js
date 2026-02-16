8fc391f526299fca9c8d403c918eaed311c7aac0
DEPD <116923032+bestkilleriranlod@users.noreply.github.com>
2026-02-16 17:08:02 +0330

    Add files via upload

--- frontend/src/utils/file-size-util.js
+++ frontend/src/utils/file-size-util.js
@@ -1,6 +1,6 @@
 const convertData = (data) => {
     const units = ['B', 'KB', 'MB', 'GB', 'TB'];
-    let size = data;
+    let size = Number(data) || 0;
     let unitIndex = 0;
 
     while (size >= 1024 && unitIndex < units.length - 1) {
